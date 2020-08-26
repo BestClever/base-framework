e753e3957d0a38809e62e68eb1621690b92f20ec
xiacong <xiacong@talkweb.com.cn>
2020-08-26 15:50:23 +0800

    初始化

--- src/main/java/com/halfsummer/baseframework/config/WebConfigurer.java
+++ src/main/java/com/halfsummer/baseframework/config/WebConfigurer.java
@@ -3,7 +3,9 @@ package com.halfsummer.baseframework.config;
 import com.halfsummer.baseframework.interceptor.LoginInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
@@ -19,7 +21,7 @@ import java.util.List;
 @Configuration
 public class WebConfigurer implements WebMvcConfigurer {
 
-    private static List<String> EXCLUDE_PATH = Arrays.asList("/","/login/login","/erro","/templates/**","/css/**","/js/**","/lib/**","/images/**","/login.html","/index.html","/pages/**","/data/**","/favicon.ico");
+    private static List<String> EXCLUDE_PATH = Arrays.asList("/","/login/login","/erro","/templates/**","/css/**","/js/**","/lib/**","/images/**","/assets/**","/component/**","/login.html","/index.html","/pages/**","/data/**","/favicon.ico","/gen/**");
 
     @Autowired
     private LoginInterceptor loginInterceptor;
@@ -27,5 +29,13 @@ public class WebConfigurer implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginInterceptor).addPathPatterns("/**").excludePathPatterns(EXCLUDE_PATH);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        //默认地址（可以是页面或后台请求接口）
+        registry.addViewController("/").setViewName("redirect:/login.html");
+        //设置过滤优先级最高
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
     }
 }
